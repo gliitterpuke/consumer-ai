f7de047e23fad6c4a81970d1225b487ccac892fb
Katherine Wang <katherinewwang@gmail.com>
2025-08-02 15:55:02 -0700

    Patch: start point

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['localhost'],
   },
@@ -10,7 +7,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:3001/api/:path*', // Backend server
+        destination: 'http://localhost:8000/api/:path*', // Backend server
       },
     ]
   },
